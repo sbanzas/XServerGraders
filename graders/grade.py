54b6d0538fde9bd2c025edd4ff27a9ade6c19156
Sebastián Banzas Baró <sebastian.banzasb@gmail.com>
2014-10-17 10:21:29 +0200

    Update grade.py

--- graders/grade.py
+++ graders/grade.py
@@ -2,15 +2,15 @@ from subprocess import *
 import os
 
 def grade(grader_path, grader_config, student_response, sandbox):
-  errors = []
-  aux = []
-  sout, serr = Popen(["/edx/app/xserver/venvs/xserver-sandbox/bin/python","-c",student_response,"-v"], stdout=PIPE, stderr=PIPE).communicate()
-  if(serr == ""):
-	correct = True
-	shrt_desc = "Script output:"
-	long_desc = " "
-	corr = True
-	aux = [[shrt_desc, long_desc, corr, " ", sout]]
+    errors = []
+    aux = []
+    sout, serr = Popen(["/edx/app/xserver/venvs/xserver-sandbox/bin/python","-c",student_response,"-v"], stdout=PIPE, stderr=PIPE).communicate()
+    if(serr == ""):
+        correct = True
+        shrt_desc = "Script output:"
+        long_desc = " "
+        corr = True
+        aux = [[shrt_desc, long_desc, corr, " ", sout]]
     else:
         correct = False
         errors = [serr,sout]
